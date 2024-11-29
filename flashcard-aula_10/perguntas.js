454dab3a935bb901f04cdd50bc9593b689e0a9bc
kauanbanin <baninred@hotmail.com>
2024-11-29 15:37:39 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação de alto nível e interpretada, usada principalmente para criar interatividade em páginas web.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a principal função do JavaScript em páginas web?',
+    'O JavaScript é usado para adicionar interatividade, manipular elementos HTML e CSS dinamicamente e criar funcionalidades como validações de formulários e animações.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é o DOM no JavaScript??',
+    'O DOM (Document Object Model) é uma interface que representa a estrutura de um documento HTML ou XML, permitindo que o JavaScript manipule e interaja com os elementos da página.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa dizer que JavaScript é uma linguagem "client-side"?',
+    'Significa que o JavaScript é executado no navegador do usuário (cliente), permitindo a manipulação da interface sem precisar de comunicação direta com o servidor'
+)
+
+criaCartao(
+    'Programação',
+    'Quais são os tipos de dados primitivos em JavaScript?',
+    'Os tipos de dados primitivos em JavaScript são: string, number, boolean, null, undefined, bigint e symbol.'
+)
+
+criaCartao(
+    'Programação',
+    'O que são eventos em JavaScript?',
+    'Eventos são ações ou ocorrências, como cliques ou movimentos do mouse, que o JavaScript pode capturar e responder usando funções específicas.'
+)
